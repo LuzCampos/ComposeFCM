2cd652961c0756bcda0bb0e1c86b8bf08c769297
LuzCampos <luzgenesis.7@gmail.com>
2022-02-26 19:09:13 -0500

    Primer commit

---
+++ app/src/main/java/com/cursodekotlin/composefcm/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.cursodekotlin.composefcm
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cursodekotlin.composefcm.ui.theme.ComposeFCMTheme
+import com.cursodekotlin.composefcm.ui.theme.Purple500
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeFCMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    FirebaseMessaging.getInstance().token
+                        .addOnCompleteListener(OnCompleteListener { task ->
+                            if (!task.isSuccessful) {
+                                Log.d("FCM Notify", "Fetching FCM registration token failed", task.exception)
+                                return@OnCompleteListener
+                            }
+
+                            //Get new FCM registration token
+                            val token: String? = task.result
+                            Log.d("FCM Token", token, task.exception)
+                            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+                        })
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Compose FCM Notification",
+                                color = Color.White,
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Purple500)
+                                    .padding(15.dp)
+                            )
+                        }
+
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.notification_image),
+                                contentDescription = "Notification Image",
+                                modifier = Modifier
+                                    .height(200.dp)
+                                    .padding(15.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
